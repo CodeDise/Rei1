c4151b2ee1c5a2cd2fb127fc791b9ec130f6184b
CODEDISE <otxstudioofficial@gmail.com>
2026-01-05 04:18:21 +0530

    Update OWNER_ID and ADMINS in config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from logging.handlers import RotatingFileHandler
 # Bot Configuration
 LOG_FILE_NAME = "bot.log"
 PORT = '5010'
-OWNER_ID = 6888478102
+OWNER_ID = 8455436361
 
 MSG_EFFECT = 5046509860389126442
 
@@ -24,7 +24,7 @@ DB_NAME = "yato"
 
 FSUBS = [[-1002490852825, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL = [-1003137762356] # just put channel id dont add ""
+DB_CHANNEL = -1003137762356 # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
 #     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
@@ -33,7 +33,7 @@ DB_CHANNEL = [-1003137762356] # just put channel id dont add ""
 # Auto Delete Timer (seconds)
 AUTO_DEL = 300
 # Admin IDs
-ADMINS = [6497757690, 6103092779]
+ADMINS = [6497757690, 8455436361]
 # Bot Settings
 DISABLE_BTN = True
 PROTECT = True
